7a678b35e661f9af048d2233a21d392425370233
basilbaby4083-max <basilbaby4083@gmail.com>
2026-03-06 22:08:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
-import ToggleMessage from "./components/ToggleMessage";
 import RunBtn from "./components/RunBtn";
 
 
 function App() {
   return <>
-  {/* <div>
-    <ToggleMessage />
-  </div> */}
-  <div>
+
     <RunBtn />
-  </div>
+
   </>;
 }
 
