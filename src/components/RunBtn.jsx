428f5c9178a51341a9c3c79665368a19057af8a0
basilbaby4083-max <basilbaby4083@gmail.com>
2026-03-27 16:30:10 +0530

    first commit

--- src/components/RunBtn.jsx
+++ src/components/RunBtn.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import "./run.css";
 
 export default function RunBtn() {
-  const [position, setPosition] = useState({ top: 770, left: 800 });
+  const [position, setPosition] = useState({ top: 480, left: 197 });
   const [message, setMessage] = useState("");
+  const [showYesImage, setShowYesImage] = useState(false);
+  const [showNoGif, setShowNoGif] = useState(false);
 
   function moveButton() {
     const newTop = Math.random() * 300;
@@ -15,20 +17,60 @@ export default function RunBtn() {
       "Why are you trying to click No? 😭",
       "Please don't do this! 🥺",
       "How many times did I say no? Please click 'Yes' 🙂",
-      "please....🥺",
-      "you dont love me?",
-      "but why"
+      "please....🥺 👉👈",
+      "you dont love me?🥺💔",
+      "but why😭",
+      "Aren’t I innocent?🙂",
+      "I will cry😢",
 
+      "Why are you so mean? 😭",
+      "Come on… just press Yes 😭",
+      "Don't break my heart 💔🥺",
+      "I trusted you 😢",
+      "Please think again 😭",
+      "You are hurting me 🥺",
+      "Why are you doing this to me 😭",
+      "Just one click on Yes please 🥺",
+      "I am begging you 🥺🙏",
+      "Please don't say No again 😭",
+      "You are making me sad 😢",
+      "I thought you liked me 🥺",
+      "My heart can't take this 💔",
+      "Please be nice 😭",
+      "Don't reject me again 🥺",
+      "You are breaking my feelings 💔",
+      "Why are you ignoring me 😭",
+      "Please give me a chance 🥺",
+      "Don't make me cry 😢",
+      "Are you really going to press No again? 😭",
+      "Think carefully before clicking 🥺",
+      "You still have time to choose Yes 🙂",
+      "Don't do this to me please 😭",
+      "I will be very sad 🥺",
+
+
+      "You are my only hope 😢",
+      "Please change your mind 😭",
+      "I believe in you 🥺",
+      "Don't destroy my happiness 💔",
+      "Why are you like this 😭",
+      
+      "Just click Yes and make me happy 🥺",
     ];
 
-    const randomMessage =
-      messages[Math.floor(Math.random() * messages.length)];
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
 
     setMessage(randomMessage);
+    setShowNoGif(true);
+    setShowYesImage(false);
   }
 
   function showMessage() {
-    setMessage("I love you too ❤️");
+    setMessage(
+      "🌹 Love you forever and always 💞 💘 You are my heart ❤️ 💎 I'm so lucky to have you 💖",
+    );
+    setShowYesImage(true);
+    setShowNoGif(false);
   }
 
   return (
@@ -37,7 +79,12 @@ export default function RunBtn() {
         <h2>Do You Love Me? 😟</h2>
         <span>Please dont click the NO nutton</span>
         <div>
-          <img src="img/Bubu-Dudu-Plushies.pngw3-Photoroom.png" alt="" />
+          <div>
+            {showNoGif && <img src="gif/tkthao219-bubududu.gif" alt="" />}
+          </div>
+          {showYesImage && (
+            <img src="img/Bubu-Dudu-Plushies.pngw3-Photoroom.png" alt="" />
+          )}
         </div>
       </div>
 
@@ -55,7 +102,8 @@ export default function RunBtn() {
             style={{
               position: "absolute",
               top: position.top,
-              left: position.left
+              left: position.left,
+              right:position.right
             }}
           >
             No
